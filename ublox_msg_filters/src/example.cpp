ec4a04b72d569e97c5ab784b7e4407d0afe1a534
Jonathan Sanabria <jonsanria@gmail.com>
2022-05-04 16:41:00 -0400

    clang formatting of files.

--- ublox_msg_filters/src/example.cpp
+++ ublox_msg_filters/src/example.cpp
@@ -5,13 +5,15 @@
 #include <ublox_msgs/NavRELPOSNED9.h>
 #include <ublox_msgs/NavVELNED.h>
 
-void callback(const ublox_msgs::NavHPPOSLLHConstPtr &msg1,
-              const ublox_msgs::NavRELPOSNED9ConstPtr &msg2,
-              const ublox_msgs::NavVELNEDConstPtr &msg3) {
+void callback(const ublox_msgs::NavHPPOSLLHConstPtr& msg1,
+              const ublox_msgs::NavRELPOSNED9ConstPtr& msg2,
+              const ublox_msgs::NavVELNEDConstPtr& msg3)
+{
   ROS_INFO("RX %u %u %u", msg1->iTOW, msg2->iTOW, msg3->iTOW);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "ublox_sync");
 
   ros::NodeHandle nh;
@@ -19,7 +21,8 @@ int main(int argc, char **argv) {
   message_filters::Subscriber<ublox_msgs::NavRELPOSNED9> sub2(nh, "msg2", 1);
   message_filters::Subscriber<ublox_msgs::NavVELNED> sub3(nh, "msg3", 1);
 
-  typedef ublox_msg_filters::ExactTime<ublox_msgs::NavHPPOSLLH, ublox_msgs::NavRELPOSNED9, ublox_msgs::NavVELNED> MySyncPolicy;
+  typedef ublox_msg_filters::ExactTime<ublox_msgs::NavHPPOSLLH, ublox_msgs::NavRELPOSNED9, ublox_msgs::NavVELNED>
+      MySyncPolicy;
   message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub1, sub2, sub3);
   sync.registerCallback(boost::bind(callback, _1, _2, _3));
 
